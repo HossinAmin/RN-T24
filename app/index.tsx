8d76bfcb824b3bae6b679dbd622519a996d2c517
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-08-16 20:49:12 +0300

    Setup project

--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,10 @@
-import Task from "@/components/Task";
-import React, { useState } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import React from "react";
+import { View, Text  } from "react-native";
 
 export default function App() {
-  const [tasks, setTasks] = useState([
-    { details: "todo app", done: false },
-    { details: "read docs", done: false },
-  ]);
-
-  const [value, setValue] = useState("");
-
-  const addTask = (value: string) => {
-    setTasks((oldValue) => [...oldValue, { details: value, done: false }]);
-  };
-
-  const handelDone = (newValue: boolean, index: number) => {
-    setTasks((preTasks) => {
-      const temp = [...preTasks];
-      temp[index].done = newValue;
-      return temp;
-    });
-  };
-
   return (
-    <View className="flex-1 p-10 items-center">
-      {tasks.map((task, index) => (
-        <Task
-          key={index}
-          task={task}
-          onDone={(value) => handelDone(value, index)}
-        />
-      ))}
-
-      <View className="w-full gap-y-2">
-        <Text>New Task:</Text>
-        <TextInput
-          className="bg-white rounded border w-full"
-          onChangeText={setValue}
-        />
-        <Button title="add task" onPress={() => addTask(value)} />
-      </View>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl">Hello</Text>
     </View>
   );
 }
