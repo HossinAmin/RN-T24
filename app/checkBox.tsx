4df645c4e809327f562be48a881ab1fc3500d738
MohamedAmr21 <ma4078458@gmail.com>
2024-08-16 21:17:39 +0300

    add to-do app

---
+++ app/checkBox.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+type CheckboxProps = {
+  isChecked: boolean;
+  onToggle: () => void;
+};
+
+export default function Checkbox({ isChecked, onToggle }: CheckboxProps) {
+  return (
+    <TouchableOpacity onPress={onToggle} className="flex-row items-center">
+      <Ionicons
+        name={isChecked ? "checkbox-outline" : "square-outline"}
+        size={24}
+        color={isChecked ? "green" : "gray"}
+      />
+    </TouchableOpacity>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,45 +1,84 @@
-import Task from "@/components/Task";
 import React, { useState } from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import { View, Text, TextInput, Button, Modal, Pressable } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import Checkbox from "./checkBox";
+
+type Todo = {
+  id: number;
+  text: string;
+  isChecked: boolean;
+};
 
 export default function App() {
-  const [tasks, setTasks] = useState([
-    { details: "todo app", done: false },
-    { details: "read docs", done: false },
-  ]);
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [todoValue, setTodoValue] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const [value, setValue] = useState("");
-
-  const addTask = (value: string) => {
-    setTasks((oldValue) => [...oldValue, { details: value, done: false }]);
+  const handleAddTodo = (): void => {
+    if (todoValue.trim()) {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: todoValue,
+        isChecked: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTodoValue("");
+      setModalVisible(false);
+    }
   };
 
-  const handelDone = (newValue: boolean, index: number) => {
-    setTasks((preTasks) => {
-      const temp = [...preTasks];
-      temp[index].done = newValue;
-      return temp;
-    });
+  const handleDeleteTodo = (id: number): void => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleCheckTodo = (id: number): void => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isChecked: !todo.isChecked } : todo
+      )
+    );
   };
 
   return (
-    <View className="flex-1 p-10 items-center">
-      {tasks.map((task, index) => (
-        <Task
-          key={index}
-          task={task}
-          onDone={(value) => handelDone(value, index)}
-        />
-      ))}
+    <View className="flex-1 p-4 justify-center items-end bg-gray-900">
+      <Ionicons
+        name="add"
+        size={32}
+        color="white"
+        onPress={() => setModalVisible(true)}
+      />
 
-      <View className="w-full gap-y-2">
-        <Text>New Task:</Text>
-        <TextInput
-          className="bg-white rounded border w-full"
-          onChangeText={setValue}
-        />
-        <Button title="add task" onPress={() => addTask(value)} />
-      </View>
+      <Modal transparent={true} visible={modalVisible} animationType="fade">
+        <View className="flex-1 justify-center items-center bg-black bg-opacity-50">
+          <View className="w-80 p-4 bg-gray-800 rounded-lg">
+            <TextInput
+              placeholder="Enter your to-do"
+              value={todoValue}
+              onChangeText={setTodoValue}
+              className="p-2 mb-4 text-white border-cyan-400"
+            />
+            <Button title="Save" onPress={handleAddTodo} />
+          </View>
+        </View>
+      </Modal>
+
+      {todos.map((todo) => (
+        <View
+          key={todo.id}
+          className="flex-row w-full items-center gap-x-2 bg-red rounded-lg m-2"
+        >
+          <Checkbox
+            isChecked={todo.isChecked}
+            onToggle={() => handleCheckTodo(todo.id)}
+          />
+          <Text className="flex-1 items-center text-white">{todo.text}</Text>
+          {todo.isChecked && (
+            <Pressable onPress={() => handleDeleteTodo(todo.id)}>
+              <Ionicons name="trash-outline" size={32} color="red" />
+            </Pressable>
+          )}
+        </View>
+      ))}
     </View>
   );
 }
