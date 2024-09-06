c1f9675d6efaf85b92dff3323caca93255252d15
MohamedAmr21 <ma4078458@gmail.com>
2024-09-06 23:25:33 +0300

    add persistant with zustand

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -1,23 +1,48 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 import * as SecureStore from "expo-secure-store";
 
-type AuthStore = {
-  isAuthenticated: boolean;
-  setToken: (token: string) => Promise<void>;
-  clearToken: () => Promise<void>;
+type AuthType = {
+  email: string;
+  password: string;
+  token: string | undefined;
+  setEmail: (newEmail: string) => void;
+  setPassword: (newPassword: string) => void;
+  setToken: (newToken: string | undefined) => void;
+  clearToken: () => void;
 };
 
-export const useAuthStore = create<AuthStore>((set) => ({
-  isAuthenticated: false,
-
-  setToken: async (token: string) => {
-    await SecureStore.setItemAsync("userToken", token);
-    set({ isAuthenticated: true });
+const secureStoreStorage = {
+  getItem: async (name: string) => {
+    return await SecureStore.getItemAsync(name);
   },
-
-  clearToken: async () => {
-    await SecureStore.deleteItemAsync("userToken");
-    set({ isAuthenticated: false });
+  setItem: async (name: string, value: string) => {
+    await SecureStore.setItemAsync(name, value);
   },
-  //    I will add a function to check if the user is authenticated getToken() and use useEffect to check if the user is authenticated on app load
-}));
+  removeItem: async (name: string) => {
+    await SecureStore.deleteItemAsync(name);
+  },
+};
+
+const useAuthStore = create(
+  persist<AuthType>(
+    (set) => ({
+      email: "",
+      password: "",
+      token: undefined,
+      setEmail: (newEmail) => set({ email: newEmail }),
+      setPassword: (newPassword) => set({ password: newPassword }),
+      setToken: (newToken) => set({ token: newToken }),
+      clearToken: async () => {
+        set({ token: undefined });
+        await SecureStore.deleteItemAsync("auth-storage");
+      },
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => secureStoreStorage),
+    }
+  )
+);
+
+export { useAuthStore };
